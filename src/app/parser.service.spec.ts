f09af4b1cab6a61ef08a03f51729d81ac903a31b
Tony Pagaduan <Elte156@gmail.com>
2022-07-26 22:32:21 -0400

    Adding helper functions

--- src/app/parser.service.spec.ts
+++ src/app/parser.service.spec.ts
@@ -1,27 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { DayUsage } from './day-usage';
+import mockData from './day-usage.fixture';
 
 import { ParserService } from './parser.service';
 
 describe('ParserService', () => {
   let service: ParserService;
-
-  const mockData: DayUsage[] = [
-    {date: new Date('07/24/2022T10:00:00'), energyHour: 1.2740, energyDay: 32.5840},
-    {date: new Date('07/24/2022T11:00:00'), energyHour: 1.4330, energyDay: 32.5840},
-    {date: new Date('07/24/2022T12:00:00'), energyHour: 1.2060, energyDay: 32.5840},
-    {date: new Date('07/24/2022T13:00:00'), energyHour: 1.7550, energyDay: 32.5840},
-    {date: new Date('07/24/2022T14:00:00'), energyHour: 0.9770, energyDay: 32.5840},
-    {date: new Date('07/24/2022T15:00:00'), energyHour: 1.0990, energyDay: 32.5840},
-    {date: new Date('07/24/2022T16:00:00'), energyHour: 1.3560, energyDay: 32.5840},
-    {date: new Date('07/24/2022T17:00:00'), energyHour: 2.5560, energyDay: 32.5840},
-    {date: new Date('07/24/2022T18:00:00'), energyHour: 1.7590, energyDay: 32.5840},
-    {date: new Date('07/24/2022T19:00:00'), energyHour: 1.5310, energyDay: 32.5840},
-    {date: new Date('07/24/2022T20:00:00'), energyHour: 1.8790, energyDay: 32.5840},
-    {date: new Date('07/24/2022T21:00:00'), energyHour: 1.5980, energyDay: 32.5840},
-    {date: new Date('07/24/2022T22:00:00'), energyHour: 1.4140, energyDay: 32.5840},
-    {date: new Date('07/24/2022T23:00:00'), energyHour: 2.0820, energyDay: 32.5840},
-  ];
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -33,10 +16,34 @@ describe('ParserService', () => {
   });
 
   it('should sum up the energy usage for a given array', () => {
-    const expected = 21.919;
+    const expected = 372.1000000000008;
 
     const actual = service.sum(mockData);
 
     expect(actual).toEqual(expected);
+  });
+
+  it('should get all items for a specific year and month from a given array', () => {
+    const expected = 24 * 8; // 24 entries per day * 8 days
+
+    const actual = service.filterByYearAndMonth(mockData, 2022, 6);
+
+    expect(actual.length).toEqual(expected);
+  });
+
+  it('should get all items for a specific day of the week from a given array', () => {
+    const expected = 48;
+
+    const actual = service.filterByDayOfWeek(mockData, 1);
+
+    expect(actual.length).toEqual(expected);
+  });
+
+  it('should get all specific hour items from a given array', () => {
+    const expected = 10; // 10 days in mock
+
+    const actual = service.filterByHourOfDay(mockData, 12);
+
+    expect(actual.length).toEqual(expected);
   });
 });
--- src/app/parser.service.ts
+++ src/app/parser.service.ts
@@ -9,8 +9,26 @@ export class ParserService {
   constructor() { }
 
   public sum(data: DayUsage[]): number {
-    return data.reduce((accumulator, object) => {
-      return accumulator + object.energyHour;
+    return data.reduce((accumulator, item) => {
+      return accumulator + item.energyHour;
     }, 0);
+  }
+
+  public filterByYearAndMonth(data: DayUsage[], year: number, month: number): DayUsage[] {
+    return data.filter((item) => {
+      return item.date.getFullYear() === year && item.date.getMonth() === month;
+    });
+  }
+
+  public filterByDayOfWeek(data: DayUsage[], dayOfWeek: number): DayUsage[] {
+    return data.filter((item) => {
+      return item.date.getDay() === dayOfWeek;
+    });
+  }
+
+  public filterByHourOfDay(data: DayUsage[], hourOfDay: number): DayUsage[] {
+    return data.filter((item) => {
+      return item.date.getHours() === hourOfDay;
+    });
   }
 }

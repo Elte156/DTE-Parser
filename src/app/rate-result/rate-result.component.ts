50d1273954dbe169af2f8b6a7eca24972e9587b6
Tony Pagaduan <Elte156@gmail.com>
2022-07-31 22:22:13 -0400

    Adding rate result presentation

---
+++ src/app/rate-result/rate-result.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-rate-result',
+  templateUrl: './rate-result.component.html',
+  styleUrls: ['./rate-result.component.scss']
+})
+export class RateResultComponent {
+  @Input()
+  title = '';
+  @Input()
+  costMonthly = 0;
+  @Input()
+  costYearly = 0;
+}

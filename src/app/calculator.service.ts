2fda9542ded93ddefee5f6d387aa2673182b4c7c
Tony Pagaduan <Elte156@gmail.com>
2022-08-03 22:32:52 -0400

    Remove calc service

--- src/app/calculator.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { ParserService } from './parser.service';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class CalculatorService {
-  constructor(protected parserService: ParserService) {}
-}

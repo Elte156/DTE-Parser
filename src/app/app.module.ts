1d27eed16cb75780b0d41710054ce812bbe62896
Tony Pagaduan <Elte156@gmail.com>
2022-08-28 22:56:55 -0400

    Angular Material (#14)
    * Installed Material
    
    * Remove boilerplate
    
    * Using material button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RateResultComponent } from './rate-result/rate-result.component';
 import { RateSectionComponent } from './rate-section/rate-section.component';
 
 @NgModule({
   declarations: [AppComponent, RateResultComponent, RateSectionComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })

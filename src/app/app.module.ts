c882ea45f8471a93c566703a1cac3e92084f3d6d
Tony Pagaduan <Elte156@gmail.com>
2022-09-07 21:30:14 -0400

    Adding snackbar for parsing complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { AppComponent } from './app.component';
 import { CardInfoComponent } from './card-info/card-info.component';
@@ -33,6 +34,7 @@ import { RateSectionComponent } from './rate-section/rate-section.component';
     MatListModule,
     MatButtonModule,
     MatIconModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],

d85c45c3d9937c23128664e12e50a153472cd2f4
Tony Pagaduan <Elte156@gmail.com>
2022-09-07 20:39:31 -0400

    Adding header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,35 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
 import { RateResultComponent } from './rate-result/rate-result.component';
 import { RateSectionComponent } from './rate-section/rate-section.component';
 
 @NgModule({
-  declarations: [AppComponent, RateResultComponent, RateSectionComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RateResultComponent,
+    RateSectionComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })

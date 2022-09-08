f73109c7e3e9720315de6012c9a434befbcdd0cd
Tony Pagaduan <Elte156@gmail.com>
2022-09-07 20:55:28 -0400

    Add info card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AppComponent } from './app.component';
+import { CardInfoComponent } from './card-info/card-info.component';
 import { HeaderComponent } from './header/header.component';
 import { RateResultComponent } from './rate-result/rate-result.component';
 import { RateSectionComponent } from './rate-section/rate-section.component';
@@ -19,11 +21,13 @@ import { RateSectionComponent } from './rate-section/rate-section.component';
     HeaderComponent,
     RateResultComponent,
     RateSectionComponent,
+    CardInfoComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    MatCardModule,
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
